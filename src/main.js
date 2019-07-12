4248213a0675eba33dff14cc96270a32aa4a0c09
Carlos <qihai323@foxmail.com>
2019-07-12 10:56:22 +0800

    好气啊

--- src/main.js
+++ src/main.js
@@ -3,26 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import YDUI from 'vue-ydui';      //移动端
+import Cube from 'cube-ui'        //适用于移动端
 import 'lib-flexible/flexible'   //px转换rem
-import 'vue-ydui/dist/ydui.rem.css';
 
+import ElementUI from 'element-ui';   //适用于PC端
+import 'element-ui/lib/theme-chalk/index.css';
 
-// import Cube from 'cube-ui'        //移动端，
-// import ElementUI from 'element-ui';   //PC端
-// import 'element-ui/lib/theme-chalk/index.css';
-
-
+// import 'lib-flexible/flexible' //px转换rem
 
 
 
 Vue.config.productionTip = false
-// Vue.use(Cube)
-// Vue.use(ElementUI)
-Vue.use(YDUI);
+Vue.use(Cube)
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
-
 new Vue({
   el: '#app',
   router,
